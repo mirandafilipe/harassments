ea71f3deaa71be9ad47ee2a6ef06156189c02cf3
Filipe Miranda <felipe_getup@hotmail.com>
2021-03-20 21:35:28 +0100

    Added first classes and rest of gradle files

---
+++ src/test/java/io/myharassment/harassments/HarassmentsApplicationTests.java
@@ -0,0 +1,13 @@
+package io.myharassment.harassments;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HarassmentsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
